a66902d052f17af616fe8b00c0fb76e8babd1044
Victor Hugo Silva <90207063+VictorHugoSDev@users.noreply.github.com>
2024-10-14 16:53:35 -0300

    Create ERF02-Contar números negativos.java

---
+++ Prova 2/Prova 2/ERF02-Contar números negativos.java
@@ -0,0 +1,8 @@
+/*
+Faça um programa em java que leia n números inteiros. Então imprima a quantidade de números negativos informados.
+
+Entrada
+ Um número inteiro n, então n números inteiros.
+
+Saída
+Imprimir a quantidade de números negativos informados.*/
