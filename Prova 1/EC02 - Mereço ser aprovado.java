5560d8c836377d12f3edbd1ff7448b741eb72ee8
Victor Hugo Silva <90207063+VictorHugoSDev@users.noreply.github.com>
2024-10-14 16:52:09 -0300

    Update EC02 - Mereço ser aprovado.java

--- Prova 1/EC02 - Mereço ser aprovado.java
+++ Prova 1/EC02 - Mereço ser aprovado.java
@@ -1,3 +1,12 @@
+/*Faça um programa em Java que leia uma nota e então calcule a porcentagem(X) dessa nota sobre o valor total da prova. Caso a nota informada seja um valor menor que 0 ou maior que 22 imprima "Nota invalida."
+
+Fórmula para calcular porcentagem : X = (nota*100)/22;
+
+Entrada
+Leia um número real(double) que corresponde a nota.
+
+Saída
+Imprima "Eu espero tirar X% da nota.". Onde X é o valor calculado pelo seu algoritmo. Caso a nota seja um valor menor que 0 ou maior que 22 imprima "Nota invalida."*/
 import java.util.Scanner;
 
 public class Main
