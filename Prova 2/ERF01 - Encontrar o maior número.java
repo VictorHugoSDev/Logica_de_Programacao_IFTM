257bd92daf4619b056320328e49e5e069e6a5894
Victor Hugo Silva <90207063+VictorHugoSDev@users.noreply.github.com>
2024-10-14 16:55:03 -0300

    Update ERF01 - Encontrar o maior número.java

--- Prova 2/ERF01 - Encontrar o maior número.java
+++ Prova 2/ERF01 - Encontrar o maior número.java
@@ -1,3 +1,10 @@
+/*Faça um programa em Java (utilizando For) que recebe inicialmente um número inteiro **n**, que representa a quantidade de números a serem fornecidos pelo usuário. Após a inserção desses **n** números, o programa deverá apresentar o maior dos números inseridos. 
+
+Entrada
+A entrada conterá um número inteiro **n** e **n** números inteiro.
+
+Saída
+Imprima o maior dos números informados.*/
 import java.util.Scanner;
 
 public class Main
