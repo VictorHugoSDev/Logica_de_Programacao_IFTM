85ab1dd9814f0df6ea3523f38739f0dd5dd01761
Victor Hugo Silva <90207063+VictorHugoSDev@users.noreply.github.com>
2024-10-14 16:55:53 -0300

    Update ERW09 - Soma de números reais.java

--- Prova 3/ERW09 - Soma de números reais.java
+++ Prova 3/ERW09 - Soma de números reais.java
@@ -1,3 +1,10 @@
+/*Faça um programa em Java para ler uma quantidade indeterminada de números reais, até a digitação de um número negativo. O programa deverá apresentar a soma desses números, bem como a quantidade de números informados. Não considerar o número negativo no cálculo, ele apenas indica ao programa a necessidade de terminar a execução.
+
+Entrada
+A entrada conterá vários números reais.
+
+Saída
+Imprima a soma e a quantidade de números informados.*/
 import java.util.Scanner;
 
 public class Main
