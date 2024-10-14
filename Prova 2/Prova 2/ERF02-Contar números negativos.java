96dfa42771039d34d64368f9ee69ccd89702d6c9
Victor Hugo Silva <90207063+VictorHugoSDev@users.noreply.github.com>
2024-10-14 16:53:57 -0300

    Update ERF02-Contar números negativos.java

--- Prova 2/Prova 2/ERF02-Contar números negativos.java
+++ Prova 2/Prova 2/ERF02-Contar números negativos.java
@@ -6,3 +6,24 @@ Entrada
 
 Saída
 Imprimir a quantidade de números negativos informados.*/
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s = new Scanner(System.in);
+	    
+	    int cont = 0;
+	    int x;
+		int n = s.nextInt();
+		
+		for(int i=0;i<n;i++){
+		    x = s.nextInt();
+		    
+		    if(x < 0){
+		        cont++;
+		    }
+		}
+		System.out.println(cont);
+	}
+}
